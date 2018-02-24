34906d4f17063e59530395e273e358ffb4866656
Sascha Homeier <sascha.homeier@gmail.com>
2018-02-24 19:22:03 +0700

    Using agea/cmis 1.0 from npm

--- src/components/LoginControl.js
+++ src/components/LoginControl.js
@@ -1,5 +1,6 @@
-import React, { Component, TextField } from 'react';
+import React, { Component } from 'react';
 import logo from '../assets/vietnam_round_icon_256.png';
+import cmis from 'cmis';
 import './login.css';
 
 class LoginControl extends Component {
@@ -7,30 +8,34 @@ class LoginControl extends Component {
         super(props);
         this.handleLoginClick = this.handleLoginClick.bind(this);
         this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = { isLoggedIn: false };
     }
 
-    handleLoginClick() {
-        this.setState({ isLoggedIn: true });
+    shouldComponentUpdate(nextProps, nextState) {
+        return false;
+    }
+
+    componentWillUpdate() {
+        console.log("Component will update")
+    }
+
+    handleLoginClick(event) {
+        event.preventDefault();
+        console.log("Test....");
+        // this.setState({ isLoggedIn: true });
+        cmisLogin();
     }
 
     handleLogoutClick() {
+        console.log("Test....");
         this.setState({ isLoggedIn: false });
     }
 
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
-
-        let div = null;
-        if (isLoggedIn) {
-            div = <LoginForm onClick={this.handleLoginClick} />;
-        } else {
-            div = <LoginForm onClick={this.handleLoginClick} />;
-        }
+        console.log("Test....");
 
         return (
             <div>
-                {div}
+                <LoginForm onClick={this.handleLoginClick} />;
             </div>
         );
     }
@@ -38,12 +43,12 @@ class LoginControl extends Component {
 
 function LoginForm(props) {
     return (
-        <div class="modal">
+        <div className="modal">
             <form className="modal-content animate">
-                <div class="imgcontainer">
-                    <img src={logo} alt="Avatar" class="avatar" />
+                <div className="imgcontainer">
+                    <img src={logo} alt="Avatar" className="avatar" />
                 </div>
-                <div class="container">
+                <div className="container">
                     <label>
                         <b>Name:</b>
                         <input type="text" name="name" />
@@ -52,13 +57,34 @@ function LoginForm(props) {
                         <b>Password:</b>
                         <input type="password" name="password" />
                     </label>
-                    <button type="submit">
+                    <button type="submit" onClick={props.onClick}>
                         Login
-                    </button>
+                        </button>
                 </div>
             </form>
         </div>
     )
+}
+
+
+function cmisLogin() {
+    console.log("Logging in.1a.. ");
+    console.log("cmis: " + cmis);
+    // var session = new cmis.CmisSession('http://localhost:8080/alfresco/api/-default-/public/cmis/versions/1.1/browser');
+    let session = new cmis.CmisSession('http://127.0.0.1:8080/alfresco/api/-default-/public/cmis/versions/1.1/browser');
+    // var session = new cmis.CmisSession('https://cmis.alfresco.com/api/-default-/public/cmis/versions/1.1/browser');
+    console.log("Logging in.1.. ");
+    session.setErrorHandler(err => console.log(err.stack));
+    session.setCredentials('admin', 'admin').loadRepositories().then((res) => {
+        console.log("Res: " + res);
+        console.log("Loaded repos");
+        console.log("Repos: " + JSON.stringify(session.defaultRepository));
+    }).catch(err => {
+        console.log("Error1: " + err)
+        console.log("Error2: " + JSON.stringify(err))
+        console.log("Error3: " + JSON.stringify(err.response))
+    }
+    );
 }
 
 export default LoginControl;
