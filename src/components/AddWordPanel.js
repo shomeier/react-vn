e72cfcd11e43cccb713f886f47e5ad4717c166de
Sascha Homeier <sascha.homeier@gmail.com>
2018-04-27 21:34:43 +0700

    Added react bootstrap Form

--- src/components/AddWordPanel.js
+++ src/components/AddWordPanel.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import cmis from 'cmis';
 // import { cmis } from '../lib/cmis';
 import './css/AddWordPanel.css';
-import { Button } from 'react-bootstrap';
+import { Button, Col, ControlLabel, Form, FormGroup, FormControl, HelpBlock } from 'react-bootstrap';
 
 export function AddWordPanel(props) {
 
@@ -10,7 +10,22 @@ export function AddWordPanel(props) {
 
     return (
         <div className="addWordPanel">
-            We are in addWord panel!
+            <Form horizontal>
+                <FormGroup controlId="WordEnglish">
+                    <Col sm={7}>
+                        <ControlLabel>English word</ControlLabel>
+                        <FormControl
+                            type="text"
+                            placeholder="Enter english word" />
+                    </Col>
+                    <Col sm={5}>
+                        <ControlLabel>Part Of Speech</ControlLabel>
+                        <FormControl
+                            type="text"
+                            placeholder="Enter part of speech" />
+                    </Col>
+                </FormGroup>
+            </Form>
         </div>
     );
 }
