a2e47e558730059c657e62973a34322362c5f3f0
Sascha Homeier <sascha.homeier@gmail.com>
2018-04-28 12:21:26 +0700

    First step to TypeScript

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require("path");
+var config = {
+  
+  devtool: 'eval',
+
+  entry: ["./src/App.tsx"],
+
+  // configure the output directory and publicPath for the devServer
+  output: {
+    filename: 'bundle.js',
+    publicPath: 'dist',
+    path: path.resolve(__dirname, "dist"),
+  },
+
+
+  // configure the dev server to run
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    inline: true,
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    // add 'src' to the modules, so that when you import files you can do so with 'src' as the relative route
+    modules: ['src', 'node_modules'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        include: path.resolve('src'),
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
