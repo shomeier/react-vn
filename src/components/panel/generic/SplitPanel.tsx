80d27d458581ed3fde52dce154ada69cebad4a94
Sascha Homeier <sascha.homeier@gmail.com>
2019-04-19 11:59:12 +0700

    Enhanced SplitPanel

--- src/components/panel/generic/SplitPanel.tsx
+++ src/components/panel/generic/SplitPanel.tsx
@@ -1,15 +1,35 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 export function SplitPanel(props) {
-    return (
-        <div className="SplitPanel">
+
+    let retVal =
+        <Col>
             <div className="SplitPanel-left">
                 {props.left}
             </div>
-            <div className="SplitPanel-center">
-                {props.center}
-            </div>
-            <div className="SplitPanel-right">
-                {props.right}
-            </div>
+        </Col>;
+    if (props.center) {
+        retVal = Object.assign(
+            <Col>
+                <div className="SplitPanel-center">
+                    {props.center}
+                </div>
+            </Col>, retVal)
+    }
+    if (props.right) {
+        retVal = Object.assign(
+            <Col>
+                <div className="SplitPanel-right">
+                    {props.right}
+                </div>
+            </Col>, retVal)
+    }
+
+    return (
+        <div className="SplitPanel">
+            <Container fluid={true}>
+                {retVal}
+            </Container>
         </div>
     );
 }
