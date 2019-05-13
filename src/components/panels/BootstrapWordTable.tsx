ef1d7ff0a9df322a95ff3cb143099aec06b1217a
Sascha Homeier <sascha.homeier@gmail.com>
2019-05-13 18:12:41 +0700

    Got Bootstrap Table 2 running

--- src/components/panels/BootstrapWordTable.tsx
+++ src/components/panels/BootstrapWordTable.tsx
@@ -1,38 +1,37 @@
 import * as React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
+import GenericeBootstrapCmisTable from '../tables/GenericBootstrapCmisTable';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
+const statement = "SELECT lingo:text, cmis:name, cmis:secondaryObjectTypeIds FROM lingo:text WHERE ANY cmis:secondaryObjectTypeIds IN ('P:lingo:word') ORDER BY lingo:text";
 const products = [{
-    id: 1,
+    succinctProperties: {id: 1},
     name: "Product1",
     price: 120
 }, {
-    id: 2,
+    succinctProperties: {id: 2},
     name: "Product2",
     price: 80
 }];
 
 const columns = [{
-    dataField: 'id',
+    dataField: 'succinctProperties[cmis:objectId]',
     text: 'Product ID'
 }, {
-    dataField: 'name',
+    dataField: 'succinctProperties[lingo:text]',
     text: 'Product Name'
 }, {
-    dataField: 'price',
+    dataField: 'succinctProperties[cmis:name]',
     text: 'Product Price'
 }];
 
 export function BootstrapWordTable() {
     return (
         <div className="container" style={{ marginTop: 50 }}>
-        <BootstrapTable 
-        striped
-        hover
-        keyField='id' 
-        data={ products } 
-        columns={ columns } />
+        <GenericeBootstrapCmisTable
+        columns={columns}
+        statement={statement} />
       </div>
     );
 }
---
+++ src/components/tables/GenericBootstrapCmisTable.tsx
@@ -0,0 +1,71 @@
+import * as React from "react"
+import { useState, useRef, useEffect } from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+import { CmisQueryService } from "../cmis/CmisQueryService";
+
+interface Props {
+    statement: string
+    columns: any
+}
+
+export default function GenericeBootstrapCmisTable(props: Props) {
+
+    const [data, setData] = useState([]);
+
+    // When sorting, filters, pageSize, or pageIndex change, fetch new data
+    useEffect(
+        () => {
+            console.log("Rerendering with statement: " + props.statement)
+            fetchData();
+        },
+        [props.statement]
+    );
+
+    const fetchData = async () => {
+        // setLoading(true);
+
+        // We can use a ref to disregard any outdated requests
+        // const id = Date.now();
+        // currentRequestRef.current = id;
+
+        // Call our server for the data
+        let filters = "";
+        let sortBy = "";
+        let pageSize = 100;
+        let pageIndex = 0;
+        const { rows, pageCount } = await CmisQueryService.getTableServerData(
+            props.statement,
+            filters,
+            sortBy,
+            pageSize,
+            pageIndex
+        );
+        console.log("Rows: " + JSON.stringify(rows))
+
+        // If this is an outdated request, disregard the results
+        // if (currentRequestRef.current !== id) {
+        //     return;
+        // }
+
+        // Set the data and pageCount
+        setData(rows);
+        // setState(old => ({
+        //     ...old,
+        //     pageCount
+        // }));
+        // console.log("STATE IN FETCH: " + JSON.stringify(props.state[0]));
+
+        // setLoading(false);
+    };
+
+    return (
+        <div>
+            <BootstrapTable
+                striped
+                hover
+                keyField='succinctProperties[cmis:objectId]'
+                data={data}
+                columns={props.columns} />
+        </div>
+    )
+}
