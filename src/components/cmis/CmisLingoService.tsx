eefc25a285f9ec99609af1c8ce8db514301f0350
Sascha Homeier <sascha.homeier@gmail.com>
2019-05-18 15:33:18 +0700

    Mark as word immediately

--- src/components/cmis/CmisLingoService.tsx
+++ src/components/cmis/CmisLingoService.tsx
@@ -25,10 +25,11 @@ export class CmisLingoService {
         try {
             const folder = CmisLingoService.BASE_FOLDER_WORDS + '/' + word.language;
             const cmisName = word.word + '_' + word.partOfSpeech;
-            const text = await this.createText(folder, cmisName, word.word)
-            const textId = text.succinctProperties["cmis:objectId"]
-            const textChangeToken = text.succinctProperties["cmis:changeToken"]
-            await this.markAs(textId, textChangeToken, CmisLingoService.WORD_MARKER)
+            // const text = await this.createText(folder, cmisName, word.word)
+            const text = await this.createWord(folder, cmisName, word.word)
+            // const textId = text.succinctProperties["cmis:objectId"]
+            // const textChangeToken = text.succinctProperties["cmis:changeToken"]
+            // await this.markAs(textId, textChangeToken, CmisLingoService.WORD_MARKER)
             return new Promise<boolean>(resolve => { resolve(true) })
         } catch (e) {
             console.log("Error while saving word: " + e)
@@ -39,6 +40,18 @@ export class CmisLingoService {
     private async createText(folder: string, cmisName: string, text: string): Promise<SuccinctCmisObject> {
         try {
             const doc_vn = await this.createDocument(folder, { "cmis:objectTypeId": "D:lingo:text", "cmis:name": cmisName, "lingo:text": text });
+            const doc_vn_id = doc_vn.succinctProperties['cmis:objectId']
+            const doc_vn_changeToken = doc_vn.succinctProperties['cmis:changeToken']
+
+            return doc_vn;
+        } catch (e) {
+            console.log("Error while creating text: " + e);
+        }
+    }
+    
+    private async createWord(folder: string, cmisName: string, text: string): Promise<SuccinctCmisObject> {
+        try {
+            const doc_vn = await this.createDocument(folder, { "cmis:objectTypeId": "D:lingo:text", "cmis:name": cmisName, "lingo:text": text, "cmis:secondaryObjectTypeIds": CmisLingoService.WORD_MARKER });
             const doc_vn_id = doc_vn.succinctProperties['cmis:objectId']
             const doc_vn_changeToken = doc_vn.succinctProperties['cmis:changeToken']
 
